254ac9af1787e4e3789fe5fb76e839c1630b42db
abdoutlem <abderrahmane.rhmari@gmail.com>
2022-04-30 11:08:14 +0200

    template de base appli django react

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./Screens/Home";
+import Contants from "expo-constants";
+import Create from "./Screens/Create";
+import AntennaDetails from "./Screens/AntennaDetails";
+import AntennaEdit from "./Screens/AntennaEdit";
 
-export default function App() {
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const myStyles = {
+  title: "Antennes deployées",
+  headerTintColor: "white",
+  headerStyle: {
+    backgroundColor: "blue",
+  },
+};
+
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Stack.Navigator>
+        <Stack.Screen name="Antenna list" component={Home} options={myStyles} />
+        <Stack.Screen
+          name="Create"
+          component={Create}
+          options={{ ...myStyles, title: "Ajouter les details de l'antenne" }}
+        />
+        <Stack.Screen
+          name="Antenna details"
+          component={AntennaDetails}
+          options={{ ...myStyles, title: "Details de l'antenne" }}
+        />
+        <Stack.Screen
+          name="Antenna Edit"
+          component={AntennaEdit}
+          options={{ ...myStyles, title: "Update the antenna" }}
+        />
+      </Stack.Navigator>
     </View>
   );
 }
 
+export default () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "gray",
+    marginTop: Contants.statusBarHeight,
   },
 });
